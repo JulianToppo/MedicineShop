9adf1d92d580d029301666b3b0be7249258f271e
JulianToppo <juliantoppo95@gmail.com>
2024-02-07 14:32:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import React, { useContext, useEffect, useState } from "react";
+import MedicineForm from "./components/MedicineForm";
+import MedicineContextStore from "./utils/MedicineContextStore";
+import Medicines from "./components/Medicines";
+import CartContextStore from "./utils/CartContextStore";
+import Cart from "./components/Cart";
+import CartContext from "./utils/CartContext";
 
 function App() {
+  const cartCtx= useContext(CartContext);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <CartContextStore>
+        <MedicineContextStore>
+          <Header />
+          <MedicineForm />
+          <Medicines />
+          <Cart/>
+        </MedicineContextStore>
+      </CartContextStore>
+    </React.Fragment>
   );
 }
 
---
+++ src/utils/constants.js
@@ -0,0 +1,1 @@
+export const crudURL="https://crudcrud.com/api/ca5e7cb9ccd14d9ea57c5dceaf54066b/"
